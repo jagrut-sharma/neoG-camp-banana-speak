417a73098c7b643c285918ab74c1b6112f8d0d3c
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-07-27 23:57:38 +0530

    Completed the JS part

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ const outputDiv = document.querySelector(".output");
 
 translateBtn.addEventListener("click", clickHandler);
 
-let urlAPI = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+let urlAPI = "https://api.funtranslations.com/translate/minion.json";
+
+let translatedtxt;
 
 function clickHandler(){
     let urlFetch = urlAPI + "?text=" + encodeURIComponent(input.value);
-    console.log(urlFetch);
+    
+    fetch(urlFetch)
+    .then(response => response.json())
+    .then(json => {
+        translatedtxt = json.contents.translated
+        outputDiv.innerText = translatedtxt})
+    .catch(errorHandler);
+}
+
+function errorHandler(error){
+    if (translatedtxt == undefined)
+    {
+        outputDiv.innerText = "Hourly limit(5 per hour) reached. Please try after an hour. Thanks";
+        console.log(error);
+    }
+    else
+    {
+        outputDiv.innerText = 'Error occured: ' + error.message;
+        console.log(error);
+    }
+
 }
