77088086e8507ed1a492a3c29f167e6851b42a09
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-07-29 23:15:48 +0530

    fixed error and style

--- app.js
+++ app.js
@@ -8,29 +8,31 @@ let urlAPI = "https://api.funtranslations.com/translate/minion.json";
 
 let translatedtxt;
 
-function clickHandler(){
+function clickHandler() {
     let urlFetch = urlAPI + "?text=" + encodeURIComponent(input.value);
-    
+
     fetch(urlFetch)
-    .then(response => response.json())
-    .then(json => {
-        translatedtxt = json;
-        outputDiv.innerText = translatedtxt.contents.translated;
-    })
-    .catch(errorHandler);
+        .then(response => response.json())
+        .then(json => {
+            translatedtxt = json;
+            outputDiv.innerText = translatedtxt.contents.translated;
+        })
+        .catch(errorHandler);
 }
 
-function errorHandler(error){
-    
-    if (translatedtxt.error.code == 429){
+function errorHandler(error) {
+
+    if (translatedtxt.error.code == 429) {
+
         outputDiv.innerText = translatedtxt.error.message
         console.log(translatedtxt.error.message);
-    }
+    } 
+    
+    else {
 
-    else{
         outputDiv.innerHTML = error + "<br>" + "<br>" + "Error from server side: " + translatedtxt.error.message + "<br>" + "<br>" + "If you have entered a new line(pressed enter) in input area, please avoid doing that, it will give error.";
-        
-        
+
+
         console.log(error);
         console.log("Error from server side: " + translatedtxt.error.message);
         console.log("If you have entered a new line(pressed enter) in input area, please avoid doing that, it will give error.");
